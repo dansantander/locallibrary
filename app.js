48e13cde51cd3175e6d4f63570349c45d578728e
dansantander <dansantandr@gmail.com>
2021-01-11 13:35:01 -0600

    chore: installed mongoose

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://dansantander:Arto$aari1@cluster0.e1juq.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
