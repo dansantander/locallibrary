8d64cd9272539927c0e97051e038625343cf5ed1
dansantander <dansantandr@gmail.com>
2021-01-15 11:30:09 -0600

    chore: add author_list controller method and view

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
 var Schema = mongoose.Schema;
 
@@ -16,6 +17,14 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  // We use fromJSDate() to import a JavaScript date string and toLocaleString()
+  // to output the date in  DATE_MED format in English: Oct 6th, 2020.
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
